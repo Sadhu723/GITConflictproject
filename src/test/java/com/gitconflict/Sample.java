11420137c3ef1c3c948f412fe89feb73f9bfdb9a
sadhu723 <hussainksatham628@gmail.com>
2022-08-29 15:31:46 +0530

    first time push

---
+++ src/test/java/com/gitconflict/Sample.java
@@ -0,0 +1,26 @@
+package com.gitconflict;
+
+public class Sample {
+   
+	private void mathod1() {
+		  System.out.println("1");
+
+		}
+		
+		private void mathod2() {
+			System.out.println("2");
+
+		}
+		
+		private void mathod3() {
+			System.out.println("3");
+
+			System.out.println("GITConflict");
+			System.out.println("GITConflict");
+			System.out.println("GITConflict");
+			System.out.println("GITConflict");
+			System.out.println("GITConflict");
+		}
+		
+		
+}
